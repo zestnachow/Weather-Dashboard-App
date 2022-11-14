3c2ccac8a39eaa205624fb9b8de7270e581934d7
Zach Weston <zachweston@Zachs-MacBook-Pro.local>
2022-11-13 22:37:58 -0700

    added second API call, grabbed lat/lon/city id from first api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,39 @@ const APIKey = "63eb4bcc2085291b819482f284bc9b49";
 
 
 // functions
-function getCity() {
+function getWeather() {
     let userInput = $("#user-input").val(); 
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey,
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey + "&units=imperial",
+    }).then(function (res) {
+        console.log(res);
+        let lat = res.coord.lat;
+        console.log(lat);
+        let lon = res.coord.lon;
+        console.log(lon);
+        let cityId = res.id;
+        console.log(cityId);
+        getForecast(cityId);
+    })
+}
+
+function getForecast(cityId) {
+    $.ajax({
+        url:  "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&appid=" + APIKey + "&units=imperial",
     }).then(function (res) {
         console.log(res);
     })
 }
 
+function convertTemp(K) {
+    return ((K-273.15)*9)/5 + 32;
+}
+
 
 // event listeners
 
-submitButton.on("click", function() {
-    getCity()
+submitButton.on("click", function(event) {
+    event.preventDefault();
+    getWeather();
 })
 
